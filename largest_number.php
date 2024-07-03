37e141fdf826598bfdb116646332e04ae592858e
Billal <billalseu@gmail.com>
2024-07-03 20:41:06 +0600

    largest number text add commit

--- largest_number.php
+++ largest_number.php
@@ -3,12 +3,13 @@
 $num1=4;
 $num2=5;
 $num3=6;
+
 if ($num1 > $num2 && $num1 > $num3) { 
-    echo "The largest number is: $num1\n"; 
+    echo "The largest number one is: $num1"; 
 } elseif ($num2 > $num1 && $num2 > $num3) { 
-    echo "The largest number is: $num2\n"; 
+    echo "The largest number two is: $num2"; 
 } else { 
-    echo "The largest number is: $num3\n"; 
+    echo "The largest number three is: $num3"; 
 } 
 
 ?>
