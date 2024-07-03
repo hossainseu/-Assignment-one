4af05a5aeef9d5e0ed8a3e72874be775c2407a14
Billal <billalseu@gmail.com>
2024-07-03 19:26:36 +0600

    first commit

--- celsius_to_fahrenheit.php
+++ celsius_to_fahrenheit.php
@@ -1,6 +1,6 @@
 <?php  
        $celsius = 32 ;  
        $fahrenheit = (($celsius*9)/5)+32 ;   
-       printf("Temperature in fahrenheit is:  %.2f",$fahrenheit);   
+       printf("Temperature in Fahrenheit is:  %.2f",$fahrenheit);   
    
 ?>
